928b56b8e3f4035c5fb2fe7f1d6c1b89f14294f6
Abraham8931 <abrahamefe307@gmail.com>
2024-11-09 16:14:10 -0800

    project added

---
+++ src/BlogDetails.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import useFetch from './usefetch';
+
+const BlogDetails = () => {
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory();
+    const handleClick = async (event) => {
+      event.preventDefault();
+    
+      try {
+        await fetch('http://localhost:8000/blogs/' + blog.id, {
+          method: 'DELETE',
+        });
+        history.push('/');
+      } catch (error) {
+        console.error('Error deleting blog:', error);
+      }
+    };
+  return (
+    <div className='blog-details'>
+        { isPending && <div>Loading...</div> }
+        { error && <div>{ error }</div> }
+        { blog && (
+            <article>
+                <h2>{ blog.title }</h2>
+                <p>written by { blog.author }</p>
+                <div>{ blog.body }</div>
+                { blog && <button onClick={handleClick}>delete</button> }
+            </article>
+        )}
+    </div>
+  )
+}
+
+export default BlogDetails;
